7aec061e419c3c0e95d617bc041393dd9d5151b7
hervedougs <herve.fehner@dougs.fr>
2022-07-04 18:30:12 +0200

    cube with demo anims + orbitControl

--- src/classes/cube.ts
+++ src/classes/cube.ts
@@ -9,23 +9,30 @@ export class Cube {
 
     static defaultSize = 0.9;
 
-
     root!: THREE.Object3D;
     material!: THREE.MeshStandardMaterial;
-    currentTween!: TWEEN.Tween<any>;
+    currentTween: TWEEN.Tween<any> | null = null;
     size!: number;
+    autoAnim = false;
 
-    constructor(private readonly scene: THREE.Scene) {
+    constructor(private readonly scene: THREE.Scene, enableDemo = true) {
         this.root = new THREE.Object3D(); 
         this.material = new THREE.MeshStandardMaterial({ color: Cube.getRandomColor(), metalness: 0.6 });
         const geometry = new THREE.BoxGeometry(Cube.defaultSize, Cube.defaultSize, Cube.defaultSize);
         const cube = new THREE.Mesh(geometry, this.material);
-        cube.position.set(0, -0.5, 0);
+        cube.position.set(0, Cube.defaultSize / 2, 0);
         this.root.add(cube);
 
         this.size = Cube.defaultSize;
 
         scene.add(this.root);
+
+        
+        if (enableDemo) {
+            this.startRotation();
+            this.autoAnim = enableDemo;
+            this.setSize(2, true, 250);
+        }
     }
 
     public setPosition(x: number, y: number, z: number) {
@@ -33,18 +40,60 @@ export class Cube {
     }
 
 
+    public setSize(size: number, withAnim = true, withDelay = 0) {
+        this.size = size;
+        if (!withAnim) {
+            this.root.scale.y = this.size;
+        } else {
+            this.animSize(withDelay);
+        }
+    }
+
+
+    private animSize(withDelay = 0): void {
+        //this.killCurrentTween();
+        if (this.root.scale.y === this.size) return; //nothing to do
+
+        const isUp = this.root.scale.y < this.size;
+        const minDuration = 1200;
+        const deltaDuration = 1800;
+        
+        this.currentTween = new TWEEN.Tween(this.root.scale)
+        .to({ y: this.size }, minDuration + Math.random() * deltaDuration)
+        .delay(withDelay)
+        .easing(isUp ? TWEEN.Easing.Elastic.Out : TWEEN.Easing.Back.In)
+        .start()
+        .onComplete(() => {
+            this.killCurrentTween();
+            if (this.autoAnim)
+                this.setSize(Cube.getRandomSize(), true, 300 + Math.random() * 500);
+        
+        });
+    }
+
+
     startRotation() {
         new TWEEN.Tween(this.root.rotation)
-            .to({ y: "-" + (Math.PI/2) * 8}, 6000)
+            .to({ y: "-" + (Math.PI/2) * 8}, 10000)
             .delay(250)
             .repeat(Infinity)
             .easing(TWEEN.Easing.Cubic.InOut)
             .start();
     }
+
+    private killCurrentTween(): void {
+        if (!this.currentTween) return;
+        this.currentTween.stop();
+        this.currentTween = null;
+    }
  
 
     static getRandomColor() {
         return availableColors[Math.floor(Math.random() * availableColors.length)];
+    }
+
+    static getRandomSize() {
+        return 0.5 + Math.random() * 5;
     }
     
     
--- src/classes/stage.ts
+++ src/classes/stage.ts
@@ -1,14 +1,17 @@
 import * as TWEEN from '@tweenjs/tween.js'
-
+import { OrbitControls } from 'three-orbitcontrols-ts';
 import { Grid } from './grid';
 import * as THREE from 'three';
 import { Cube } from './cube';
+
+
 
 export class Stage {
 
     scene!: THREE.Scene;
     renderer!: THREE.WebGLRenderer;
     camera!: THREE.PerspectiveCamera;
+    controls!: OrbitControls;
 
     cubePivot!: THREE.Object3D;
 
@@ -20,24 +23,28 @@ export class Stage {
 
     initRenderer(): void {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
 
-        this.camera.position.z = 5;
+        this.camera.position.set(-5, 5, -5);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement );
+        this.controls.enableZoom = true; // useless for now... 
+        this.controls.enableKeys = true; // same here. Need to check that.
+        this.controls.autoRotate = true;
 
         this.initLights();
     }
 
     initLights(): void {
-        const light = new THREE.AmbientLight(0x404040, 2.5); // soft white light
+        const light = new THREE.AmbientLight(0x404040, 3); // soft white light
         this.scene.add(light);
 
-        const directionalLight = new THREE.DirectionalLight(0x52C8F2, 2);
+        const directionalLight = new THREE.DirectionalLight(0x52C8F2, 2.5);
         directionalLight.castShadow = true;
-        directionalLight.position.set(3, 3, 3);
+        directionalLight.position.set(-3, 3, -3);
         this.scene.add(directionalLight);
     }
 
@@ -53,6 +60,7 @@ export class Stage {
     render = () => {
         requestAnimationFrame(this.render);
         this.renderer.render(this.scene, this.camera);
+        this.controls.update();
         TWEEN.update();
     }
 }
