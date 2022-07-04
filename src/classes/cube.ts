102a6a1a1ddbc63878ff5000e35b481575add317
hervedougs <herve.fehner@dougs.fr>
2022-07-04 16:24:19 +0200

    first stage render + cube with tween test

--- src/classes/cube.ts
+++ src/classes/cube.ts
@@ -1,1 +1,36 @@
-export class Cube {}
+import * as THREE from 'three';
+import * as TWEEN  from '@tweenjs/tween.js'
+
+
+export class Cube {
+
+    public root!: THREE.Object3D;
+    material!: THREE.MeshStandardMaterial;
+
+    constructor(private readonly scene: THREE.Scene) {
+        this.root = new THREE.Object3D(); 
+        this.material = new THREE.MeshStandardMaterial({ color: 0x52C8F2, metalness: 0.6 });
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const cube = new THREE.Mesh(geometry, this.material);
+        cube.position.set(0, -0.5, 0);
+        this.root.add(cube);
+
+        scene.add(this.root);
+
+        this.startRotation();
+    }
+
+
+    startRotation() {
+        new TWEEN.Tween(this.root.rotation)
+            .to({ y: "-" + (Math.PI/2) * 8}, 6000)
+            .delay(250)
+            .repeat(Infinity)
+            .easing(TWEEN.Easing.Cubic.InOut)
+            .start();
+    }
+ 
+    
+    
+
+}
